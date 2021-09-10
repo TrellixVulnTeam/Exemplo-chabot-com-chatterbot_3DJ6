14ff553eaeece21d0a066625048d69b646fa92fc
Leandro Santana <31331005+leandrosantana09@users.noreply.github.com>
2021-09-10 17:38:08 -0300

    Update chat_procon.py

--- chat_procon.py
+++ chat_procon.py
@@ -15,9 +15,8 @@ from dados import pergunta_custo, pergunta_custo1, pergunta_custo2, pergunta_cus
 # Criando o chat
 chatbot = ChatBot('Chat - PROCON',
                   read_only=False,
-                  statement_comparison_function=compare,
-                  # statement_comparison_function=JaccardSimilarity,
-                  response_selection_method=get_most_frequent_response,
+                  #statement_comparison_function=compare,
+                  #response_selection_method=select,
                   logic_adapters=[
                       {
                           "import_path": "chatterbot.logic.BestMatch",
@@ -29,17 +28,12 @@ chatbot = ChatBot('Chat - PROCON',
 
 trainer = ListTrainer(chatbot)
 
-for pergunta, resposta in zip(pergunta_custo, resposta_custo):
-    trainer.train([pergunta, resposta])
-
-for pergunta1, resposta in zip(pergunta_custo1, resposta_custo):
-    trainer.train([pergunta1, resposta])
-
-for pergunta2, resposta in zip(pergunta_custo2, resposta_custo):
-    trainer.train([pergunta2, resposta])
-
-for pergunta3, resposta in zip(pergunta_custo3, resposta_custo):
-    trainer.train([pergunta3, resposta])
+trainer.train(["Oi", "Olá, tudo bem ?", 
+               "Tudo bem!", "Que bom",
+               'Olá, tudo bem?', 'Tudo bem sim, e você?',
+               "Como você esta?", "Estou bem, melhor agora!",
+               "Onde é a barraca de pão de queijo?", "Opa, você está no lugar certo!"
+               ])  
 
 trainer.train(
     "chatterbot.corpus.portuguese"
@@ -49,15 +43,15 @@ trainer.train(
 #loop para a conversa
 
 name = input("Opa! Tudo bem? Posso saber o seu nome ?")
-print('Seja bem vindo {}. Aqui podemos te ajudar a verificar os melhores preços de Itajubá - MG! '.format(name))
+print('Oi {}. Vamos conversar? '.format(name))
 
 while True:
     try:
         response = (chatbot.get_response(input(name + ":")))
         if float(response.confidence) > 0.7:
-            print("Chat - PROCON: ", response.text)
+            print("Chat: ", response.text)
         else:
-            print('Chat - PROCON: Poderia por favor refazer a sua pergunta ?')
+            print('Chat: Poderia por favor refazer a sua pergunta ?')
             print(response.confidence)
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
